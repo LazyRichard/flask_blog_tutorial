46e8076a15ec5e1950af59942131904de0187e18
Suh. Junmin <sof.midnight@live.co.kr>
2018-08-06 05:27:35 +0900

    add chapter_2-2

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
+from datetime import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': {
+            'username': 'test-user'
+        },
+        'title': '첫 번째 포스트',
+        'content': '첫 번째 포스트 내용입니다.',
+        'date_posted': datetime.strptime('2018-08-01', '%Y-%m-%d')
+    },
+    {
+        'author': {
+            'username': 'test-user'
+        },
+        'title': '두 번째 포스트',
+        'content': '두 번째 포스트 내용입니다.',
+        'date_posted': datetime.strptime('2018-08-03', '%Y-%m-%d')
+    },
+]
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template('about.html', title='About')
